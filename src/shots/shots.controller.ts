1d5268928205b19fc58ef9e0706be541b9fe46ad
lnart <lennart.pafel@code.berlin>
2025-01-09 21:22:13 +0100

    FEAT: write shots module controller and service

---
+++ src/shots/shots.controller.ts
@@ -0,0 +1,78 @@
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post } from '@nestjs/common';
+import { ShotsService } from './shots.service';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateShotDto, EditShotDto, ReadShotDto } from 'src/dto/shots.dto';
+
+@ApiTags('shots')
+@Controller('shots')
+export class ShotsController {
+  constructor(private readonly ShotsService: ShotsService) {}
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get a Shot' })
+  @ApiResponse({
+    status: 200,
+    description: 'Shot Successfully Read',
+    type: ReadShotDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async get(@Param('id') id: string): Promise<ReadShotDto> {
+    return await this.ShotsService.get(id);
+  }
+
+  @Get('many/:userId')
+  @ApiOperation({ summary: 'Get all Shots' })
+  @ApiResponse({
+    status: 200,
+    description: 'Shots Successfully Read',
+    type: [ReadShotDto],
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async getAll(@Param('userId') userId: string): Promise<ReadShotDto[]> {
+    const shots = await this.ShotsService.getAll(userId);
+    return shots;
+}
+
+  @Post('one')
+  @ApiOperation({ summary: 'create one Shot' })
+  @ApiBody({ type: CreateShotDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Shot Successfully Created',
+    type: ReadShotDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async create(@Body() createShotDto: CreateShotDto) {
+    return await this.ShotsService.create(createShotDto);
+  }
+
+  @Patch('one/:id')
+  @ApiOperation({ summary: 'Edit one shot' })
+  @ApiBody({ type: EditShotDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Shot Successfully edited',
+    type: ReadShotDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async edit(@Body() editShotDto: EditShotDto, @Param("id") id:string) {
+    return await this.ShotsService.edit(editShotDto, id);
+  }
+
+  @Delete(':id')
+  @ApiOperation({summary: 'Delete One Shot'})
+  @ApiResponse({
+    status: 201,
+    description: 'Shot Successfully Deleted',
+    type: ReadShotDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async delete(@Param("id") id:string): Promise<ReadShotDto>{
+    return await this.ShotsService.delete(id)
+  }
+}
--- src/shots/shots.module.ts
+++ src/shots/shots.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ShotsService } from './shots.service';
+import { ShotsController } from './shots.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Shot } from 'src/data/entities/shot.entity/shot.entity';
+import { ConfigModule} from '@nestjs/config';
+import { DatabaseModule } from 'src/data/database.module';
 
 @Module({
-  providers: [ShotsService]
+  imports: [TypeOrmModule.forFeature([Shot]), DatabaseModule, ConfigModule],
+  providers: [ShotsService],
+  controllers: [ShotsController],
+  exports: [ShotsService],
 })
 export class ShotsModule {}
