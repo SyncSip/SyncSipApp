82350ae22a1ac5784061315db1a699d560b51924
lnart <lennart.pafel@code.berlin>
2025-01-29 16:33:28 +0100

    FEAT: use bluetooth context

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { BluetoothProvider } from '@/components/BluetoothContext';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -27,6 +28,8 @@ export default function RootLayout() {
   }
 
   return (
+    <BluetoothProvider>
+
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -34,5 +37,6 @@ export default function RootLayout() {
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
+    </BluetoothProvider>
   );
 }
