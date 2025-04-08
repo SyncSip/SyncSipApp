d94c8a18796efc04c54d267d1288e9f74f9f017b
lnart <lennart.pafel@code.berlin>
2025-04-08 18:30:48 +0200

    FEAT: adjust bean dto

--- Backend/src/dto/bean.dto.ts
+++ Backend/src/dto/bean.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, IsUUID, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, IsUUID, IsOptional, IsBoolean, IsDate, IsArray } from 'class-validator';
+import { CustomField } from 'src/data/entities/shot.entity/shot.entity';
 
 export class CreateBeanDto {
     @IsString()
@@ -17,6 +18,80 @@ export class CreateBeanDto {
         example: 'Red Brick',
     })
     bean: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "The altitude of the coffee farm in meters",
+        example: '1000',
+        required: false
+    })
+    altitudeInMeters?: string;
+
+    @IsDate()
+    @IsOptional()
+    @ApiProperty({
+        description: "The date this bean was roasted",
+        example: new Date(),
+        required: false
+    })
+    roastDate?: Date;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "process",
+        example: 'Washed',
+        required: false
+    })
+    process?: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "Genetic",
+        example: 'Arabica',
+        required: false
+    })
+    genetic?: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "Variety",
+        example: 'Maragogype',
+        required: false
+    })
+    variety?: string;
+
+    @IsArray()
+    @IsOptional()
+    @ApiProperty({
+        description: "Customfields",
+        example: [
+            {key: "frozen", value: 'yes'}
+        ],
+        required: false
+    })
+    customFields?: CustomField[];
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "The origin country of the coffee",
+        example: 'Kenya',
+        required: false
+    })
+    origin?: string;
+
+    @IsBoolean()
+    @IsOptional()
+    @ApiProperty({
+        description: "Indicates if a bag of coffee is empty or not",
+        example: 'true',
+        required: false
+    })
+    full?: boolean;
 
     @IsUUID()
     @IsNotEmpty()
@@ -62,6 +137,70 @@ export class ReadBeanDto {
         format: 'uuid'
     })
     userId: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "The altitude of the coffee farm in meters",
+        example: '1000',
+    })
+    altitudeInMeters: string;
+
+    @IsDate()
+    @IsOptional()
+    @ApiProperty({
+        description: "The date this bean was roasted",
+        example: '01.04.2025',
+    })
+    roastDate: Date;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "process",
+        example: 'Washed',
+    })
+    process: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "Genetic",
+        example: 'Arabica',
+    })
+    genetic: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "Variety",
+        example: 'Maragogype',
+    })
+    variety: string;
+
+    @IsArray()
+    @IsOptional()
+    @ApiProperty({
+        description: "Customfields",
+        example: [{key:"frozen", value: "yes"}],
+    })
+    customFields: CustomField[];
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "The origin country of the coffee",
+        example: 'Kenya',
+    })
+    origin: string;
+
+    @IsBoolean()
+    @ApiProperty({
+        description: "Indicates if a bag of coffee is empty or not",
+        example: 'true',
+    })
+    full: boolean;
+
 }
 
 export class EditBeanDto {
@@ -82,6 +221,79 @@ export class EditBeanDto {
         required: false
     })
     bean?: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "The altitude of the coffee farm in meters",
+        example: '1000',
+        required: false
+    })
+    altitudeInMeters?: string;
+
+    @IsDate()
+    @IsOptional()
+    @ApiProperty({
+        description: "The date this bean was roasted",
+        example: '01.04.2025',
+        required: false
+    })
+    roastDate?: Date;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "process",
+        example: 'Washed',
+        required: false
+    })
+    process?: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "Genetic",
+        example: 'Arabica',
+        required: false
+    })
+    genetic?: string;
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "Variety",
+        example: 'Maragogype',
+        required: false
+    })
+    variety?: string;
+
+    @IsArray()
+    @IsOptional()
+    @ApiProperty({
+        description: "Customfields",
+        example: [{key:"frozen", value: "yes"}],
+        required: false
+    })
+    customFields?: CustomField[];
+
+    @IsString()
+    @IsOptional()
+    @ApiProperty({
+        description: "The origin country of the coffee",
+        example: 'Kenya',
+        required: false
+    })
+    origin?: string;
+
+    @IsBoolean()
+    @IsOptional()
+    @ApiProperty({
+        description: "Indicates if a bag of coffee is empty or not",
+        example: 'true',
+        required: false
+    })
+    full?: boolean;
+
 }
 
 export class DeleteBeanDto {
